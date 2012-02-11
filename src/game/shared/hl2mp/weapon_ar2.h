9b07c0685d8444857474e3f098b298052956fac8
mcwattersandrew <mcwattersandrew@gmail.com>
2012-02-11 23:17:27 +0000

    Added missing declarations during weapon_ar2 code merge

--- src/game/shared/hl2mp/weapon_ar2.h
+++ src/game/shared/hl2mp/weapon_ar2.h
@@ -41,12 +41,27 @@ public:
 
 	void	AddViewKick( void );
 
+#ifdef HL2SB
+#ifndef CLIENT_DLL
+	void	FireNPCPrimaryAttack( CBaseCombatCharacter *pOperator, bool bUseWeaponAngles );
+	void	FireNPCSecondaryAttack( CBaseCombatCharacter *pOperator, bool bUseWeaponAngles );
+	void	Operator_ForceNPCFire( CBaseCombatCharacter  *pOperator, bool bSecondary );
+	void	Operator_HandleAnimEvent( animevent_t *pEvent, CBaseCombatCharacter *pOperator );
+#endif
+#endif
+
 	int		GetMinBurst( void ) { return 2; }
 	int		GetMaxBurst( void ) { return 5; }
 	float	GetFireRate( void ) { return 0.1f; }
 
 	bool	CanHolster( void );
 	bool	Reload( void );
+
+#ifdef HL2SB
+#ifndef CLIENT_DLL
+	int		CapabilitiesGet( void ) { return bits_CAP_WEAPON_RANGE_ATTACK1; }
+#endif
+#endif
 
 	Activity	GetPrimaryAttackActivity( void );
 	
